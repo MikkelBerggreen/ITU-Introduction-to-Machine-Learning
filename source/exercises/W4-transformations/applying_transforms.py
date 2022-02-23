30a8d43aede297d4d47212ea1bd7581cf6bc6ca1
Anton Mølbjerg Eskildsen <antonmeskildsen@me.com>
2022-02-23 14:54:28 +0100

    update

--- source/exercises/W4-transformations/applying_transforms.py
+++ source/exercises/W4-transformations/applying_transforms.py
@@ -47,6 +47,8 @@ def load_or_create_affine(video, image_map):
         # Exercise
         points_source = Counter(image_ground, "Ground points").get_points(3)
         points_destination = Counter(image_map, "Map points").get_points(3)
+        # Replace this with your solution
+        Hgm = None
 
         video.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
