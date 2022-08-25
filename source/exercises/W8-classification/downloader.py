4fbd8d84ff0717516994e346e2d0b1a3ff8d65f5
Anton Mølbjerg Eskildsen <antonmeskildsen@me.com>
2022-08-25 18:16:52 +0200

    rollback for fall 2022

--- source/exercises/W8-classification/downloader.py
+++
@@ -1,9 +0,0 @@
-import os
-from torchvision.datasets import MNIST, FashionMNIST
-
-dir = os.path.join(os.getcwd(), 'data')
-
-MNIST(dir, train=True, download=True)
-MNIST(dir, train=False, download=True)
-FashionMNIST(dir, train=True, download=True)
-FashionMNIST(dir, train=False, download=True)
